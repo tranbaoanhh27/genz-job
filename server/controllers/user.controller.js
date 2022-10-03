6e624e40bd9e4603ae3ada1a07d9c21634eaeecf
Thế Anh <theanhbr01@gmail.com>
2022-10-03 13:14:57 +0700

    CRUD example without middleware

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -15,7 +15,9 @@ router.post('/', async (req, res, next) => {
     await db.User.create({
         userName: req.body.username,
         email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, 8)
+        password: bcrypt.hashSync(req.body.password, 8),
+        createdDate: Date.now(),
+        lastActivyDate: Date.now()
       })
       .then(user => {
         res.send("User Created");
@@ -25,4 +27,60 @@ router.post('/', async (req, res, next) => {
       });
       
 });
+
+router.put('/', async (req, res, next) => {
+    var user = db.User.findOne({
+        where: {
+          userName: req.body.username
+        }
+    })
+    .then(user => {
+        if (!user) {
+          return res.status(404).send({ message: "User Not found." });
+        }
+
+        db.User.update({
+            userName: req.body.username,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 8),
+            lastActivyDate: Date.now()
+        }, {
+            where: {
+                id: user.id
+            }
+        })
+        .then(user => {
+            res.send("User Updated");
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message });
+        });
+    });
+});
+
+router.delete('/', async (req, res, next) => {
+    var user = db.User.findOne({
+        where: {
+          userName: req.body.username
+        }
+    })
+    .then(user => {
+        if (!user) {
+          return res.status(404).send({ message: "User Not found." });
+        }
+
+        db.User.destroy({
+            where: {
+                id: user.id
+            }
+        })
+        .then(user => {
+            res.send("User Deleted");
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message });
+        });
+    });
+});
+
 module.exports = router
