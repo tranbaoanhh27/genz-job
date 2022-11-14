01a33e96b26563bd6ad29f44572242f676f7d8e2
tranbaoanhh27 <tranbaoanhh27@gmail.com>
2022-11-14 20:19:37 +0700

    Fix bugs (at least it can run without error :'))

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import './index.css';
+import {createRoot} from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App';
 
