0794ef7e0526bc5b099a4fcb7557846be9c08c08
Tris Lee <quangtri2002lk@gmail.com>
2022-12-23 22:51:04 +0700

    complete message

--- client/src/components/Messages/shell/Chat-Shell.js
+++ client/src/components/Messages/shell/Chat-Shell.js
@@ -16,11 +16,11 @@ const firebase = initializeApp(FirebaseConfig);
 const db = getDatabase(firebase);
 
 const ChatShell = (props) => {
-    const [selectedConversationIdx, setSelectedConversationIdx] = useState(0);
-    const [selectedConversation, setSelectedConversation] = useState();
+    const [selectedConversation, setSelectedConversation] = useState(null);
+    const [messages, setMessages] = useState([]);
     const [conversations, setConversations] = useState([]);
-    const [receiverId, setReceiverId] = useState(props.user.data.id);
-    const [senderId, setSenderId] = useState("1");
+    const userId = props.user.data.id;
+    const username = props.user.data.UserName;
   
     useEffect(() => {
         const resp = ref(db, process.env.REACT_APP_FIREBASE_CHAT_COLLECTION);
@@ -28,61 +28,65 @@ const ChatShell = (props) => {
         return onValue(resp, (snap) => {
             if (snap.exists()) {
                 const data = snap.val();
-            var messageArray = Object.keys(data)
-            .map(k => ({
-                ...data[k],
-            })); 
-            messageArray.sort((a, b) => {
-                return b.timestamp - a.timestamp;
-            });
-            
-            let conversationList = [];
-            messageArray.forEach(item => {
-                var tmp = conversationList.find(c => 
-                    (c.senderId === item.senderId && c.receiverId === item.receiverId) || (c.senderId === item.receiverId && c.receiverId === item.senderId));
-                    if ((item.senderId === receiverId || item.receiverId === receiverId) 
-                    && !tmp) {
+                var messageArray = Object.keys(data)
+                .map(k => ({
+                    ...data[k],
+                })); 
+                messageArray.sort((a, b) => {
+                    return b.timestamp - a.timestamp;
+                });
+                
+                let conversationList = [];
+                let messagesList = [];
+                messageArray.forEach(item => {
+                    var tmp = conversationList.find(c => 
+                        (c.senderId === item.senderId && c.receiverId === item.receiverId) || (c.senderId === item.receiverId && c.receiverId === item.senderId));
+                    if ((item.senderId === userId || item.receiverId === userId) && !tmp) {
                         conversationList.push(item);
                     }
-                });
 
+                    if (selectedConversation) {
+                        if ((item.senderId === selectedConversation.senderId && item.receiverId === selectedConversation.receiverId) || 
+                            (item.senderId === selectedConversation.receiverId && item.receiverId === selectedConversation.senderId)) {
+                                messagesList.push(item);
+                            }
+                    }
+                });
                 setConversations(conversationList);
-            }
-            let selectedConver = conversations[selectedConversationIdx];
-            if (selectedConver) {
-                if (selectedConver.senderId === receiverId) {
-                    setSenderId(selectedConver.receiverId);
-                }
-                else {
-                    setSenderId(selectedConver.senderId);
-                }
-                selectedConver.title = senderId;
-                setSelectedConversation(selectedConver);
-            }
-            else {
-                selectedConver = [];
-                selectedConver.title = senderId;
-                setSelectedConversation(selectedConver);
+                setMessages(messagesList);
             }
       });
     }, [conversations]);
 
-    const sendMessageCallback = () => {
-        setSelectedConversationIdx(0);
-    };
+    let chatReceiverId = "";
+    let chatReceiverName = "";
+    if (selectedConversation) {
+        chatReceiverId = selectedConversation.receiverId;
+        chatReceiverName = selectedConversation.receiverName;
+        if (selectedConversation.receiverId === userId) {
+            chatReceiverId = selectedConversation.senderId;
+            chatReceiverName = selectedConversation.senderName;
+        }
+    }
 
     return (
         <div id="chat-container" style={{fontSize: "50%"}}>
             {/* <ConversationSearch /> */}
             <ConversationList 
-                receiverId={receiverId}
+                userId={userId}
                 conversations={conversations} 
-                selectedConversationIdx={selectedConversationIdx}
-                setSelectedConversationIdx={setSelectedConversationIdx}/>
-            <NewConversation setSenderId={setSenderId}/>
+                setSelectedConversation={setSelectedConversation}
+                selectedConversation={selectedConversation}/>
+            <NewConversation 
+                userId={userId}
+                username={username}
+                setSelectedConversation={setSelectedConversation}/>
             {selectedConversation && <ChatTitle selectedConversation={selectedConversation} />}
-            {selectedConversation && <MessageList messages={messages} senderId={senderId} receiverId={receiverId}/>}
-            {selectedConversation && <ChatForm senderId={senderId} receiverId={receiverId} sendMessageCallback={sendMessageCallback}/>}
+            {selectedConversation && <MessageList messages={messages} userId={userId} receiverId={selectedConversation.receiverId}/>}
+            {selectedConversation && <ChatForm senderId={userId} 
+                                               receiverId={chatReceiverId} 
+                                               senderName={username}
+                                               receiverName={chatReceiverName}/>}
         </div>
     );
 }
