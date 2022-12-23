f2592b434e831be277a777c77a948d74a5265130
tranbaoanhh27 <tranbaoanhh27@gmail.com>
2022-12-23 18:37:09 +0700

    implemented jobs searching feature in Recruiter's My Jobs

--- client/src/components/Recruiter/Job/Jobs.js
+++ client/src/components/Recruiter/Job/Jobs.js
@@ -6,24 +6,29 @@ import RecruiterJobSearch from "./JobSearch";
 
 const RecruiterJobs = (props) => {
     const [sortMode, setSortMode] = useState("sortByTitle"); // oldestFirst or latestFirst or sortByTitle
+    const [keyword, setKeyword] = useState(undefined); // for searching jobs
 
+    // Sort jobs
     let sortedJobs = [...props.jobs];
-
     let compareFunction = undefined;
-    if (sortMode === "latestFirst") compareFunction = (a, b) => b.createdDate - a.createdDate;
-    else if (sortMode === "oldestFirst") compareFunction = (a, b) => a.createdDate - b.createdDate;
-    else if (sortMode === "sortByTitle")
-        compareFunction = (a, b) => {
-            return a.title.localeCompare(b.title);
-        };
-
+    const compareFunctionDecisions = {
+        latestFirst: (a, b) => b.createdDate - a.createdDate,
+        oldestFirst: (a, b) => a.createdDate - b.createdDate,
+        sortByTitle: (a, b) => a.title.localeCompare(b.title),
+    };
+    compareFunction = compareFunctionDecisions[sortMode];
     if (compareFunction !== undefined) sortedJobs = sortedJobs.sort(compareFunction);
+
+    // Filter jobs by keyword
+    let filteredJobs = [...sortedJobs];
+    if (keyword !== undefined)
+        filteredJobs = filteredJobs.filter((job) => job.title.includes(keyword));
 
     return (
         <Column>
-            <RecruiterJobSearch />
+            <RecruiterJobSearch onChangeKeyword={(keyword) => setKeyword(keyword)} />
             <RecruiterJobFilter onChangeSortMode={(sortMode) => setSortMode(sortMode)} />
-            <JobList jobs={sortedJobs} onSelectJob={props.onSelectJob} />
+            <JobList jobs={filteredJobs} onSelectJob={props.onSelectJob} />
         </Column>
     );
 };
