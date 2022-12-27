6db2ed36e2ba70e35109aeb2750cff987b2a1bfd
tranbaoanhh27 <tranbaoanhh27@gmail.com>
2022-12-27 22:03:42 +0700

    fix bug CardJob

--- client/src/api/Bookmark.js
+++ client/src/api/Bookmark.js
@@ -1,21 +1,24 @@
 import axios from "axios";
 
 export function createBookmark(userId, jobId, resolve, reject) {
-
     let apiUrl = process.env.REACT_APP_API_URL;
 
-    axios.post(apiUrl + `/bookmark/create?userId=${userId}&jobId=${jobId}`, {}).then(resolve).catch(reject);
+    axios
+        .post(apiUrl + `/bookmark/create?userId=${userId}&jobId=${jobId}`, {})
+        .then(resolve)
+        .catch(reject);
 }
 
-export function checkIsBookmarked(userId, jobId, resolve) {   
-
+export function checkIsBookmarked(userId, jobId, resolve) {
     let apiUrl = process.env.REACT_APP_API_URL;
 
-    axios.get(apiUrl + `/bookmark?userId=${userId}`).then( (responde) => {
+    if (!userId || !jobId) return;
+
+    axios.get(apiUrl + `/bookmark?userId=${userId}`).then((responde) => {
         let listJobBookmarked = responde.data;
-        let result = listJobBookmarked.some( (curVal) => {
+        let result = listJobBookmarked.some((curVal) => {
             return curVal.JobId === jobId;
-        })
+        });
         resolve(result);
-    })
+    });
 }
